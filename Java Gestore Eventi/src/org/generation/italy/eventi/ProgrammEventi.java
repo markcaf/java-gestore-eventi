0cc34dac3983ed5c065a246f672f63115f44db14
Marco Cafasso <marcocafassog@gmail.com>
2022-11-25 16:37:30 +0100

    feat: create ProgrammEventi class with attributes
    ● title
    ● events list
    ● constructor
    ● getters and setters

---
+++ Java Gestore Eventi/src/org/generation/italy/eventi/ProgrammEventi.java
@@ -0,0 +1,29 @@
+package org.generation.italy.eventi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammEventi {
+	
+	private String titolo;
+	private List<Evento> eventi;
+	
+	public ProgrammEventi(String titolo) {
+		setTitolo(titolo);
+		this.eventi = new ArrayList<>();
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public List<Evento> getEventi() {
+		return eventi;
+	}
+	
+	
+}
