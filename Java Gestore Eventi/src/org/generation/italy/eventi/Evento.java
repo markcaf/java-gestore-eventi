9facc062da5a61be3fc38d8605daea8584d7f01f
Marco Cafasso <marcocafassog@gmail.com>
2022-11-25 10:36:10 +0100

    feat: add constructor, getters and setters
    Attributes valued in the constructor, except reserved seats which is initialized to 0.
    Inserted the check that the date has not already passed and that the total number of seats is positive. Otherwise raise appropriate exceptions.

--- Java Gestore Eventi/src/org/generation/italy/eventi/Evento.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Evento.java
@@ -8,5 +8,44 @@ public class Evento {
 	private LocalDate data;
 	private int postiTotali;
 	private int postiPrenotati;
+	
+	public Evento(String titolo, LocalDate data, int postiTotali) throws Exception {
+		setTitolo(titolo);
+		setData(data);
+		if(postiTotali < 0) {
+			throw new Exception("Il numero di posti totali non può essere inferiore a 0");
+		}
+		this.postiTotali = postiTotali;
+		this.postiPrenotati = 0;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) throws Exception {
+		if(data.isBefore(LocalDate.now())) {
+			throw new Exception("La data inserita non è valida");
+		}
+		this.data = data;
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+	
+	
 
 }
