64dd047265fb49d90fcafc1600cc1372ba81bc8f
Marco Cafasso <marcocafassog@gmail.com>
2022-11-25 15:35:30 +0100

    feat(Main): Milestone 2 completed
    After the event has been instantiated, ask the user if and how many reservations/cancellations they want to make and try to make them, implementing appropriate controls and managing any exceptions.
    Finally, print the number of seats booked and those available on the screen.

--- Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
@@ -19,10 +19,74 @@ public class Main {
 		System.out.print("\nInserisci il numero di posti totali dell'evento: ");
 		int postiTotali = sc.nextInt();
 		
-		sc.close();
 		System.out.println("\n-------------------------------\n");
-		Evento eventoUtente = new Evento(titolo, data, postiTotali);
-		System.out.println(eventoUtente);
+		
+		//Evento eventoUtente = new Evento(titolo, data, postiTotali);
+		//System.out.println(eventoUtente);
+		
+		Evento eventoUtente = null;
+		try {
+			eventoUtente = new Evento(titolo, data, postiTotali);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			System.out.println(eventoUtente == null ? "L'evento non può essere creato" : eventoUtente);
+		}
+		
+		if (eventoUtente != null) {
+			boolean exit = false;
+			
+			while(!exit) {
+				
+				System.out.println("\n-------------------------------\n");
+			
+				System.out.println("Scegli un opzione per l'operazione da svolgere"
+						+ "\n1. Prenota evento"
+						+ "\n2. Disdici evento"
+						+ "\n3. Termina operazioni");
+				int sceltaUtente = sc.nextInt();
+				
+				
+				if (sceltaUtente == 1) {
+					System.out.print("Inserisci il numero di prenotazioni da effettuare: ");
+					int daPrenotare = sc.nextInt();
+					for (int i=0; i < daPrenotare; i++) {
+						try {
+							eventoUtente.prenota();
+						} catch (Exception e) {
+							System.err.println(e.getMessage());
+						}
+						
+						eventoUtente.prenota();
+					}
+					int postiDisponibili = eventoUtente.getPostiTotali() - eventoUtente.getPostiPrenotati();
+					System.out.println("\nTotale posti prenotati: " + eventoUtente.getPostiTotali());
+					System.out.println("Totale posti disponibili: " + postiDisponibili);
+				}
+				
+				if (sceltaUtente == 2) {
+					System.out.print("Inserisci il numero di prenotazioni da disdire: ");
+					int daDisdire = sc.nextInt();
+					for (int i=0; i < daDisdire; i++) {
+						try {
+							eventoUtente.disdici();
+						} catch (Exception e) {
+							System.err.println(e.getMessage());
+						}
+					}
+					int postiDisponibili = eventoUtente.getPostiTotali() - eventoUtente.getPostiPrenotati();
+					System.out.println("\nTotale posti prenotati: " + eventoUtente.getPostiPrenotati());
+					System.out.println("Totale posti disponibili: " + postiDisponibili);
+				}
+				
+				if (sceltaUtente == 3 || sceltaUtente != 1 && sceltaUtente != 2) {
+					System.out.print("\nTermine operazioni.");
+					exit = true;
+				}
+				
+			}
+		}
+		
 		
 	}
 
