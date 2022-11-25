a5145d226d22e529a6c4f8b8d7b266937dbc6d88
Marco Cafasso <marcocafassog@gmail.com>
2022-11-25 09:55:07 +0100

    feat: create Evento class with attributes
    ● title
    ● date
    ● number of seats in total
    ● number of seats booked

---
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Evento.java
@@ -0,0 +1,12 @@
+package org.generation.italy.eventi;
+
+import java.time.LocalDate;
+
+public class Evento {
+	
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+
+}
