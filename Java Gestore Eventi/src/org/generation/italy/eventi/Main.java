4270def5cf3144830542d686a5e3707e86c682dd
Marco Cafasso <marcocafassog@gmail.com>
2022-11-25 12:23:56 +0100

    feat(Main): instantiated a new obj Evento with user input
    test in which the user is asked to enter a new event with all parameters

--- Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
@@ -1,11 +1,29 @@
 package org.generation.italy.eventi;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.print("Inserisci il titolo dell'evento: ");
+		String titolo = sc.nextLine();
+		
+		System.out.print("\nInserisci la data dell'evento (Anno-Mese-Giorno): ");
+		String dataStr = sc.next();
+		LocalDate data = LocalDate.parse(dataStr);
+		
+		System.out.print("\nInserisci il numero di posti totali dell'evento: ");
+		int postiTotali = sc.nextInt();
+		
+		sc.close();
+		System.out.println("\n-------------------------------\n");
+		Evento eventoUtente = new Evento(titolo, data, postiTotali);
+		System.out.println(eventoUtente);
+		
 	}
 
 }
